8d4b24473a29d3bafef56a9665430ce2f00aa05a
JohnyDaison <johnydaison@gmail.com>
2022-04-10 14:08:27 +0200

    initial project commit

---
+++ Source/ThirdPersonMP/ThirdPersonMPGameMode.cpp
@@ -0,0 +1,10 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "ThirdPersonMPGameMode.h"
+#include "ThirdPersonMPCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+
+AThirdPersonMPGameMode::AThirdPersonMPGameMode()
+{
+	
+}
